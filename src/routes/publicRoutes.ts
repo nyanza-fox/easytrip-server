6eee2b7dd66f97527b46891c8ca394f5933a1f56
Hascine <thehascine@gmail.com>
2024-06-13 00:40:49 +0700

    Feat: Setup Public Endpoints (BE) - public routes done

--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -1,16 +1,22 @@
 import express from 'express';
 
 import destinationController from '../controllers/destinationController';
+import transportationController from '../controllers/transportationController';
+import accommodationController from '../controllers/accommodationController';
+import guideController from '../controllers/guideController';
 
 const router = express.Router();
 
 router.get('/destinations', destinationController.getAllDestinations);
 router.get('/destinations/:id', destinationController.getDestinationById);
-router.get('/transportations');
-router.get('/transportations/:id');
-router.get('/accomodations');
-router.get('/accomodations/:id');
-router.get('/guides');
-router.get('/guides/:id');
+router.get('/transportations', transportationController.getAllTransportations);
+router.get(
+  '/transportations/:id',
+  transportationController.getTransportationById
+);
+router.get('/accomodations', accommodationController.getAllAccommodations);
+router.get('/accomodations/:id', accommodationController.getAccommodationById);
+router.get('/guides', guideController.getAllGuides);
+router.get('/guides/:id', guideController.getGuideById);
 
 export default router;

d4316659469380bf60522254e9bd00844fb4c9d7
simsonhutagaol <simsonhutagaol58@gmail.com>
2024-06-17 16:49:29 +0700

    update order and payment

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,10 +1,9 @@
 import { NextFunction } from "express";
-
 import orderModel from "../models/orderModel";
-
 import type { CustomRequest, CustomResponse } from "../types/express";
 import { ObjectId } from "mongodb";
 import { OrderInput } from "../types/order";
+import stripe from "../lib/stripe";
 
 const orderController = {
   getAllOrders: async (
@@ -73,6 +72,7 @@ const orderController = {
       next(error);
     }
   },
+
   createOrderAndPayment: async (
     req: CustomRequest,
     res: CustomResponse,
@@ -80,14 +80,51 @@ const orderController = {
   ) => {
     try {
       const data: OrderInput = req.body;
-      const id = req.user?.id;
-      data.userId = new ObjectId(id);
+      data.userId = new ObjectId("6669d6aae384c5bf46d885cb");
       const order = await orderModel.create(data);
+      if (!order) {
+        return;
+      }
+      const guide = await orderModel.findById(order.insertedId.toHexString());
+      const priceIDR: any = guide?.package.totalPrice;
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: "Paket Liburan",
+              },
+              unit_amount: 3 * 100,
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: `http://localhost:3000/complete?session_id=${order.insertedId}`,
+        cancel_url: `http://localhost:3000/cancel?session_id=${order.insertedId}`,
+      });
       res.status(200).json({
         statusCode: 200,
         message: "Order retrieved successfully",
-        data: order,
+        data: session.url,
       });
+    } catch (error) {
+      next(error);
+    }
+  },
+  updateStatus: async (
+    req: CustomRequest,
+    res: CustomResponse,
+    next: NextFunction
+  ) => {
+    try {
+      const data = req.body;
+      const order = await orderModel.updateStatus(data.id, data.status);
+      if (!order) {
+        return;
+      }
     } catch (error) {
       next(error);
     }
